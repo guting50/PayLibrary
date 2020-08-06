1247b411a596df40af798eb55c169413d20c4007
GuTing <goodguting@gmail.com>
2020-08-06 10:55:58 +0800

    no message

--- app/src/main/java/com/gt/pay/wxapi/WXPayEntryActivity.java
+++ app/src/main/java/com/gt/pay/wxapi/WXPayEntryActivity.java
@@ -3,9 +3,6 @@ package com.gt.pay.wxapi;
 import common.pay.sdk.PayEntryActivity;
 
 /**
- * User: fee(1176610771@qq.com)
- * Date: 2016-07-05
- * Time: 13:48
  * DESC: 微信那撕必须要该类名的Activity
  * 在自己的APP内的wxapi包名下写一个继承CommonPaySdk的PayEntryActivity的WxPayEntryActivity即可
  */
